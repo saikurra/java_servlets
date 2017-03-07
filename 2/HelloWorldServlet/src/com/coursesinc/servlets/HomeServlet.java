7a6b97e38e6be8f9d50d4f0a7973c4cbeba7c719
Kurra <859223@corpaa.aa.com>
2017-03-07 10:13:32 -0600

    Redirect changed to request forward

--- 2/HelloWorldServlet/src/com/coursesinc/servlets/HomeServlet.java
+++ 2/HelloWorldServlet/src/com/coursesinc/servlets/HomeServlet.java
@@ -2,16 +2,20 @@ package com.coursesinc.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HomeServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
-		response.sendRedirect("views/home.jsp");
+			throws IOException, ServletException {
+		/*PrintWriter writer = response.getWriter();
+		
+		writer.append("<h1>Sai's Dashboard</h1>");
+		writer.append("<h4>I am a software engineer in American Airlines</h4>");*/
+		
+		request.getRequestDispatcher("views/home.jsp").forward(request, response);;
 	}
 }
